e4ca24da26425514219ec6d06d46bad70a2bd075
ROSMIEZA92 <rosmieza@maxsys.com.my>
2026-05-26 11:46:03 +0800

    change from [controller] to 'auth' in the AuthController.cs

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 namespace CLE_BackEnd.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/auth")]
 public class AuthController : ControllerBase
 {
     private readonly IAuthService _authService;
